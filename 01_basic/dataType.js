27bf9469d25d15925a6979f59e19a7bd0178bb21
Sachin-MERN <154312889+Sachin-MERN@users.noreply.github.com>
2024-02-27 17:01:03 +0000

    data type of js

---
+++ 01_basic/dataType.js
@@ -0,0 +1,25 @@
+let fullName = "sachin madas"; // this is called string datatype
+console.log(fullName);
+let age = 30; // this is call number datatype
+console.log(age);
+
+// following are the datatype in javascript 
+
+/*
+ 1 string ----> always write in the ("double quetos")
+ 2 number -----> always write directly 123
+ 3 Boolean -----> always return the true or false value 
+ 4 binint ------> if you want to write large number morw then 2 power 53 then we can use this 
+ 5 symbol ------> for unique id 
+ 6 undefine -----> means value not assign it 
+ 7 null --------> empty it not queal to undefine both are deffernt 
+*/
+
+// objects
+
+console.log(typeof Boolean);
+console.log(typeof undefined );
+console.log(typeof null);
+console.log(typeof NaN);
+
+
